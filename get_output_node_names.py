08e1baee6ad2f1572e5dcde572e6f81cedaa6c96
Nguyễn Trần Minh Tuấn <ngtranminhtuan@gmail.com>
2021-10-08 23:08:30 +0700

    Add files via upload

---
+++ get_output_node_names.py
@@ -0,0 +1,28 @@
+import tensorflow as tf
+
+def load_graph(frozen_graph_filename):
+    with tf.io.gfile.GFile(frozen_graph_filename, "rb") as f:
+        graph_def = tf.compat.v1.GraphDef()
+        graph_def.ParseFromString(f.read())
+    with tf.Graph().as_default() as graph:
+        tf.import_graph_def(graph_def)
+    return graph
+
+def analyze_inputs_outputs(graph):
+    ops = graph.get_operations()
+    outputs_set = set(ops)
+    inputs = []
+    for op in ops:
+        if len(op.inputs) == 0 and op.type != 'Const':
+            inputs.append(op)
+        else:
+            for input_tensor in op.inputs:
+                if input_tensor.op in outputs_set:
+                    outputs_set.remove(input_tensor.op)
+    outputs = list(outputs_set)
+    return (inputs, outputs)
+   
+graph = load_graph('/home/tuan/Downloads/SLAM/DMP_SLAM/dmp_slam_ros_ws/bin/models/perception/yolov4.pb')
+inputs, outputs = analyze_inputs_outputs(graph)
+print(inputs)
+print(outputs)
